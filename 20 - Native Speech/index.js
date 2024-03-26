112a3108956c244a8d7b6efb48fa15a94c2672a7
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-03-26 19:35:31 +0100

    add day 20 files

--- 20 - Native Speech/index.js
+++ 20 - Native Speech/index.js
@@ -1,2 +1,28 @@
 window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+    p.textContent = transcript;
+    if (e.result[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    if (transcript.includes('Get the weather')) {
+        // You can run a function on voice command
+        console.log(`☀️☀️☀️☀️☀️☀️☀️☀️☀️ 🌧️🌧️🌧️🌧️🌧️`);
+    }
+});
+recognition.addEventListener('end', recognition.start());
+
+recognition.start();
